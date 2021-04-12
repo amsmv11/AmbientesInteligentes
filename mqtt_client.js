f3fdaa49c58baa7ef2fcc5bfdb9be3657892eb72
António Vieira <antoniovieira@manjaro.lan>
2021-04-12 16:03:52 +0100

    mqtt updated js

--- mqtt_client.js
+++ mqtt_client.js
@@ -1,48 +1,180 @@
 // // npm install mqtt --save
 // // npm install mqtt -g
-try{
-    var mqtt = require('mqtt');
-
-    options = {
-        clientId:"mqttjs01",
-        username:"steve",
-        password:"password",
-        clean:true
-    };
-
-    const client = mqtt.connect("mqtt://broker.emqx.io");
-    // "broker.emqx.io"
-    // var client = mqtt.connect("broker.emqx.io", options);
-
-
-    client.on('message', function(topic, message, packet) {
-        console.log("message is " + message);
-        console.log("topic is " + topic);
-    });
-
-
-    client.on("connect", function() {
-        console.log("connected");
-    });
-
-    var topic_list=["AmbInt/sensors/pot","AmbInt/sensors/temp","AmbInt/sensors/int"];
-    client.subscribe(topic_list,{qos:1});
-    // var topic_o = {"AmbInt/sensors/pot":1,"AmbInt/sensors/temp":1,"AmbInt/sensors/int":1};
-    // client.subscribe(topic_o);
-
-
-
-    //handle errors
-    client.on("error",function(error){
-        console.log("Can't connect" + error);
-        process.exit(1)
-    });
-} catch (e) {
-    console.log(e);
-    client.end();
-}
-
+const getUpdate = (setState) => {
+    var client;
   
+    try{
+        var mqtt = require('mqtt');
+  
+        const options = {
+            clientId:"mqttjs01",
+            username:"steve",
+            password:"password",
+            clean:true
+        };
+  
+        client = mqtt.connect("mqtt://broker.emqx.io");
+        // "broker.emqx.io"
+        // var client = mqtt.connect("broker.emqx.io", options);
+  
+  
+        client.on('message', function(topic, message, packet) {
+            console.log("message is " + message);
+            setState(message);
+            console.log("topic is " + topic);
+        });
+  
+  
+        client.on("connect", function() {
+            console.log("connected");
+        });
+  
+        var topic_list=["AmbInt/sensors/pot","AmbInt/sensors/temp","AmbInt/sensors/int"];
+        client.subscribe(topic_list,{qos:1});
+        // var topic_o = {"AmbInt/sensors/pot":1,"AmbInt/sensors/temp":1,"AmbInt/sensors/int":1};
+        // client.subscribe(topic_o);
+  
+  
+  
+        //handle errors
+        client.on("error",function(error){
+            console.log("Can't connect" + error);
+            process.exit(1)
+        });
+    } catch (e) {
+        console.log(e);
+    } finally {
+      client.end();
+    }
+  
+    
+  
+  };
+  
+  export default getUpdate;
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// REACT NATIVE VERSION
+import React, { useState } from 'react';
+import { TouchableOpacity, Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
+
+
+
+const MqttClient = () => {
+  var client;
+  const [state, setState] = useState("");
+
+  try{
+      var mqtt = require('mqtt');
+
+      const options = {
+          clientId:"mqttjs01",
+          username:"steve",
+          password:"password",
+          clean:true
+      };
+
+      client = mqtt.connect("mqtt://broker.emqx.io");
+      // "broker.emqx.io"
+      // var client = mqtt.connect("broker.emqx.io", options);
+
+
+      client.on('message', function(topic, message, packet) {
+          console.log("message is " + message);
+          setState(message);
+          console.log("topic is " + topic);
+      });
+
+
+      client.on("connect", function() {
+          console.log("connected");
+      });
+
+      var topic_list=["AmbInt/sensors/pot","AmbInt/sensors/temp","AmbInt/sensors/int"];
+      client.subscribe(topic_list,{qos:1});
+      // var topic_o = {"AmbInt/sensors/pot":1,"AmbInt/sensors/temp":1,"AmbInt/sensors/int":1};
+      // client.subscribe(topic_o);
+
+
+
+      //handle errors
+      client.on("error",function(error){
+          console.log("Can't connect" + error);
+          process.exit(1)
+      });
+  } catch (e) {
+      console.log(e);
+  } finally {
+    client.end();
+  }
+
+  return (<Text style={styles.paragraph}>
+            {state}
+          </Text>);
+
+};
+
+export default MqttClient;
+
+
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
